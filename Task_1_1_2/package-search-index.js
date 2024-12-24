a29cffbfdc3a6a6b38a49902c2667ac1a300d1d6
RostyanYandexLyceumCheeeeck <RostyanYandexLyceumCheeeeck@users.noreply.github.com>
2024-12-24 03:11:44 +0000

    Deploying to gh-pages from @ RostyanYandexLyceumCheeeeck/OOP@2e891a9732b53e9150cf8dfe6a7d4b966b484138 🚀

--- Task_1_1_2/package-search-index.js
+++ Task_1_1_2/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.syspro.zagitov.blackjack"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.syspro.zagitov.blackjack"},{"l":"ru.nsu.syspro.zagitov.blackjack.cards"}];updateSearchResults();
